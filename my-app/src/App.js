beddb5feb698acd768dcb78f990c3809a5d65fdf
yilechang <yilechang@gmail.com>
2020-07-23 16:37:47 +0800

    feat: first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,100 @@
+import React, { PureComponent, forwardRef } from 'react';
+import axios from 'axios';
+import './App.css';
+
+import MaterialTable from "material-table";
+// import material icon
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+class App extends PureComponent {
+  state = {
+    data: [],
+  };
+  componentDidMount(){
+    axios
+      .get('https://data.taipei/api/v1/dataset/1f1aaba5-616a-4a33-867d-878142cac5c4?scope=resourceAquire')
+      .then(({ data: res }) => {
+        console.log(res)
+        this.setState({data: res.result.results})
+      });
+  }
+  render() {
+    const { data } = this.state
+      return (
+      <div className="App">
+        <MaterialTable
+          columns={[
+            { title: "dataTime", field: "dataTime" },
+            { title: "geocode", field: "geocode" },
+            { title: "lat", field: "lat" },
+            { title: "locationName", field: "locationName" },
+            { title: "lon", field: "lon" },
+            { title: "measures", field: "measures" },
+            { title: "value", field: "value" },
+          ]}
+          data={data}
+          title="Demo Title"
+          options={{
+            filtering: true
+          }}
+          icons={tableIcons}
+        />
+      </div>
+    );
+  }
+}
+// function App() {
+  // return (
+  //   <div className="App">
+  //     hello
+  //     {/* <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header> */}
+  //   </div>
+  // );
+// }
+
+export default App;
