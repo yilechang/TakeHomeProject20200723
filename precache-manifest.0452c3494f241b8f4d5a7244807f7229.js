fbfd4137b0c1bd7655ebc1e8d24f83a23ca99170
yilechang <yilechang@gmail.com>
2020-07-23 16:48:40 +0800

    Updates

---
+++ precache-manifest.0452c3494f241b8f4d5a7244807f7229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfea082106564d0f988b2b10be40b5ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "312a5ced02d5a91b7fc8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8144a30563113795078",
+    "url": "/static/js/2.ea94b696.chunk.js"
+  },
+  {
+    "revision": "c856daee926a1ac6d00671f46b678ccc",
+    "url": "/static/js/2.ea94b696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312a5ced02d5a91b7fc8",
+    "url": "/static/js/main.43471bbc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
